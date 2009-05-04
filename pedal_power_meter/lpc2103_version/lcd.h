20b5d47d9ad091a7dc36a83aa48eeaee7eb39213
casainho <casainho@a328dbd0-25c3-11de-b186-ef1da62b6547>
2009-05-04 21:10:43 +0000

    LCD drivers are working now.

---
+++ pedal_power_meter/lpc2103_version/lcd.h
@@ -0,0 +1,27 @@
+#define		CLR_DISP	   0x00000001
+#define		DISP_ON		   0x0000000C
+#define		DISP_OFF	   0x00000008
+#define		CUR_HOME       0x00000002
+#define		CUR_OFF 	   0x0000000C
+#define     CUR_ON_UNDER   0x0000000E
+#define     CUR_ON_BLINK   0x0000000F
+#define     CUR_LEFT       0x00000010
+#define     CUR_RIGHT      0x00000014
+#define		CUR_UP  	   0x00000080
+#define		CUR_DOWN	   0x000000C0
+#define     ENTER          0x000000C0
+#define		DD_RAM_ADDR    0x00000080
+#define		DD_RAM_ADDR2   0x000000C0
+
+void E_Pulse();
+void LCDInit();
+void LCDSendCommand(unsigned char byte);
+void SmartUp(void);
+void SmartDown(void);
+void LCDSendChar(unsigned char byte);
+void LCDSendInt(long number, unsigned char number_of_digits);
+void LCDSendStr(unsigned char *string);
+void LCDSendFloat(double number, unsigned char number_of_digits, \
+        unsigned char number_of_floats);
+
+
--- pedal_power_meter/lpc2103_version/main.c
+++ pedal_power_meter/lpc2103_version/main.c
@@ -1,27 +1,64 @@
 #include "lpc210x.h"
+#include "main.h"
 #include "system.h"
-#include "timers.h"
+#include "lcd.h"
 
 int main (void)
 {
-    int j;
+    unsigned char str_volts[] = " Volts";
+    unsigned char str_amps[] = " Amps";
+
+    volatile double voltage_temp, current_temp;
+
+    /* Initialize variables */
+    voltage             = 0;
+    current             = 0;
+    wattage             = 0;
+    wattage_hour        = 0;
+    tick_update_lcd     = 0;
 
 	/* Initialize the system */
     system_init ();
 
-	//timer1_init ();
+    /* Initialize the LCD */
+    LCDInit();
+
+    /* Initialize the Timer0 */
+    //timer1_init ();
+    //enableIRQ ();
+
+    /* Endless blink loop */
+    while (1)
+    {
+        //if (tick_update_lcd >= 50)
+        //{
+
+        LCDSendCommand(DD_RAM_ADDR); /* LCD set first row */
+        LCDSendChar('1');
+        LCDSendChar('2');
 
 
-	// set io pins for led P0.20
-	IODIR |= (1 << 20);	// pin P0.20 is an output
-	IOCLR =  (1 << 20);	// led on
+        //LCDSendCommand(DD_RAM_ADDR); /* LCD set first row */
+            //voltage_temp = (double) adc_read(0);
+            /* voltage_temp = (double) 511;
 
-	// endless loop to toggle the red  LED P0.26
-	while (1)
-	{
-        IOCLR = (1 << 20);
-        for (j = 0; j < 500000; j++ );		// wait 500 msec
-        IOSET = (1 << 20);
-        for (j = 0; j < 300000; j++ );
-	}
+            LCDSendInt (voltage_temp, 4);
+            LCDSendChar (' ');
+            LCDSendChar (' ');
+            LCDSendChar (' ');
+            LCDSendFloat (voltage_temp * k_voltage, 2, 1); */
+
+
+            //LCDSendCommand(DD_RAM_ADDR2);
+            //voltage_temp = (double) adc_read(1);
+            //LCDSendInt (current_temp, 4);
+            //LCDSendChar (' ');
+            //LCDSendChar (' ');
+            //LCDSendChar (' ');
+            //LCDSendFloat (current_temp * k_current, 2, 1);
+
+
+            //tick_update_lcd = 0;
+        //}
+    }
 }
