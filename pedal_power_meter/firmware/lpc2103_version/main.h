7ea5792f87bc29293bd37793c283de56aaf8a724
casainho <casainho@a328dbd0-25c3-11de-b186-ef1da62b6547>
2009-05-18 22:18:51 +0000

    Moved the firmware directories to a main firmware directory.

---
+++ pedal_power_meter/firmware/lpc2103_version/main.h
@@ -0,0 +1,9 @@
+#define K_VOLTAGE 0.036690647
+#define K_CURRENT 0.02
+
+#define MENU_SHOW_POWER 0
+#define MENU_SHOW_VOLTAGE_CURRENT 1
+
+/* Global variables */
+double voltage, current, wattage, wattage_hour;
+short tick_update_lcd, nr_adc_reads;
