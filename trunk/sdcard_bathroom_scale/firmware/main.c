33c22e359ced9d0ba04667f2037716c45054d44b
casainho <casainho@a328dbd0-25c3-11de-b186-ef1da62b6547>
2009-07-26 14:27:30 +0000

    Added not tested yeat FAT and SD Card drivers.

--- trunk/sdcard_bathroom_scale/firmware/main.c
+++ trunk/sdcard_bathroom_scale/firmware/main.c
@@ -12,22 +12,55 @@
 #include "lpc210x.h"
 #include "main.h"
 #include "system.h"
+#include "isrsupport.h"
 #include "lcd.h"
 #include "timers.h"
 #include "ios.h"
-#include "isrsupport.h"
+#include "spi.h"
+#include "./fatfs/diskio.h"
+#include "./fatfs/ff.h"
+#include "err.h"
+
+#ifndef NULL
+#define NULL    0
+#endif
 
 /* Global variables */
 unsigned long int           back_plane_a,
                             back_plane_b,
                             back_plane_c;
 
+/*---------------------------------------------------------*/
+/* User Provided RTC Function for FatFs module             */
+/*---------------------------------------------------------*/
+/* This is a real time clock service to be called from     */
+/* FatFs module. Any valid time must be returned even if   */
+/* the system does not support an RTC.                     */
+/* This function is not required in read-only cfg.         */
+
+
+unsigned long get_fattime ()
+{
+    /* Pack date and time into a DWORD variable */
+    return    ((unsigned long)(2009 - 1980) << 25)
+            | ((unsigned long) 01 << 21)
+            | ((unsigned long) 01 << 16)
+            | ((unsigned long) 0 << 11)
+            | ((unsigned long) 0 << 5)
+            | ((unsigned long) 0 >> 1);
+}
+
 int main (void)
 {
     /* Initialize variables */
     volatile unsigned char temp_char = 0;
-    volatile float temp_float = 0;
+    volatile float weight = 0;
     unsigned char state = 0;
+
+    FATFS fs;       /* Work area (file system object) for logical drive */
+    FIL file;       /* file object */
+    FRESULT res;    /* FatFs function common result code */
+
 
 	/* Initialize the system */
     system_init ();
@@ -38,10 +71,13 @@ int main (void)
     /* Initialize the LCD */
     lcd_init ();
 
-    /* Initialize the Timer1 */
+    spi_init ();
 
-    timer1_init ();
+    /* Initialize the Timers */
+    timer0_init ();
+    //timer1_init ();
     enableIRQ ();
+
 
     while (1)
     {
@@ -60,12 +96,12 @@ int main (void)
             back_plane_a = get_ios ();
 
             lcd_send_command (DD_RAM_ADDR); /* LCD set first row */
-            temp_float = get_weight (back_plane_a, back_plane_b, back_plane_c);
+            weight = get_weight (back_plane_a, back_plane_b, back_plane_c);
             lcd_send_char (' ');
             lcd_send_char (' ');
             lcd_send_char (' ');
             lcd_send_char (' ');
-            lcd_send_float (temp_float, 3, 1);
+            lcd_send_float (weight, 3, 1);
             lcd_send_char (' ');
             lcd_send_char ('K');
             lcd_send_char ('g');
@@ -78,13 +114,50 @@ int main (void)
             switch (state)
             {
                 case 0:
-                timer1_register (100000);
+                timer1_register (50000);
                 state = 1;
                 break;
 
                 case 1:
                 if (!timer1_run)
+                {
+                   /* If weight are higher than 40kg, store it on the SD Card */
+                    if (weight > 40)
+                    {
+                        /* Register a work area for logical drive 0 */
+                        res = f_mount(0, &fs);
+                        if (res)
+                            die ("Err mount fs");
+
+                        /* Open source file */
+                        res = f_open(&file, "weight.csv", FA_OPEN_ALWAYS | FA_WRITE);
+                        if (res)
+                            die ("Err create file");
+
+                        /* Move to end of the file to append data */
+                        res = f_lseek(&file, file.fsize);
+
+                        /* Write the weight value at end of file with a CSV */
+                        f_printf(&file, "%d", (int) weight);
+                        f_printf(&file, "%c", ';');
+
+                        /* Close the file */
+                        f_close(&file);
+                        if (res)
+                            die ("Err close file");
+
+                        /* Unregister a work area before discard it */
+                        res = f_mount(0, NULL);
+                        if (res)
+                            die ("Err unmount fs");
+                    }
+
+
+                    lcd_send_string ("Weight saved");
+                    timer1_register (2500000); /* wait 2,5 seconds */
+                    while (timer1_run) ;
                     lcd_send_command (CLR_DISP);
+                }
                 break;
             }
         }
