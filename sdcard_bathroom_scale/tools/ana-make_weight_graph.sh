3f5e65838658ba6c792b3a905f1a96dc5b739d80
casainho <casainho@a328dbd0-25c3-11de-b186-ef1da62b6547>
2009-10-06 14:01:16 +0000

    Added Gnuplot and Python scripts to make correctly the weight graphs.

---
+++ sdcard_bathroom_scale/tools/ana-make_weight_graph.sh
@@ -0,0 +1,2 @@
+python ./scripts/weight_csv2gnuplot.py w_ana.csv
+gnuplot ./scripts/gnuplot_weight-ana
---
+++ sdcard_bathroom_scale/tools/jorge-make_weight_graph.sh
@@ -0,0 +1,2 @@
+python ./scripts/weight_csv2gnuplot.py w_jorge.csv
+gnuplot ./scripts/gnuplot_weight-jorge
